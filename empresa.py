5f83df319b744617aa8be5b4bcf1550e1adbb3db
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-27 11:47:20 -0300

    Enhance Streamlit app with images and title
    Added images and title to the Streamlit app.

--- empresa.py
+++ empresa.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+st.image("exmplo.png", width=300)
+st.title("empresa")
+
+st.imagem("exemp.png", width=100)
+
+st.image("whatsapp-logo.png", width=100, link="https://wa.me/+5583981821591")
