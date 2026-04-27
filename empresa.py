caa6a1b3ca70b670291ad8ac1f1e521ed893c253
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-27 11:53:25 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.image("exmplo.png", width=300)
-st.title("empresa")
+st.image("samsung-logo.png", width=300, link="https://www.samsung.com/br/")
+st.title("Samsung")
 
 st.imagem("exemp.png", width=100)
+st.write("")
 
 st.image("whatsapp-logo.png", width=100, link="https://wa.me/+5583981821591")
