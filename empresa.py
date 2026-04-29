e0923e0590ea3d974dc67319c4c08ea04238ef22
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-29 11:02:45 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -3,8 +3,13 @@ import streamlit as st
 st.image("samsung-logo.png", width=300, link="https://www.samsung.com.br/")
 st.title("Samsung")
 
-st.image("Panda-Vermelho.png", width=100)
-st.write("Sobre Mim:")
-st.write("Olá")
+col1, col2 = st.columns(2)
+
+with col1:
+  st.image("Panda-Vermelho.png", width=100)
+  
+with col2:
+  st.header("Sobre Mim:")
+  st.write("Olá")
 
 st.image("whatsapp-logo.png", width=100, link="https://wa.me/+5583981821591")
