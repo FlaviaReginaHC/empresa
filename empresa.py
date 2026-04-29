431a6a571f35604647c90efa36123ee4482fed8a
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-29 10:54:56 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.image("samsung-logo.png", width=300)
 st.title("Samsung")
 
-st.imagem("Panda-Vermelho.png", width=100)
+st.image("Panda-Vermelho.png", width=100)
 
 st.image("whatsapp-logo.png", width=100, link="https://wa.me/+5583981821591")
