025a934d474bd051242b6d7a46f83c226e4620c0
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-29 11:14:37 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -12,7 +12,7 @@ st.markdown("<br>", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 
 with col1:
-  st.image("Panda-Vermelho.png", width=100)
+  st.image("Panda-Vermelho.png", width=300)
   
 with col2:
   st.header("Sobre Mim:")
