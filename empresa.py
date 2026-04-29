1c654545d84394e6cbf882a2d1b67f354420ff04
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-29 10:58:35 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -4,5 +4,7 @@ st.image("samsung-logo.png", width=300, link="https://www.samsung.com.br/")
 st.title("Samsung")
 
 st.image("Panda-Vermelho.png", width=100)
+st.write("Sobre Mim:")
+st.write("Olá")
 
 st.image("whatsapp-logo.png", width=100, link="https://wa.me/+5583981821591")
