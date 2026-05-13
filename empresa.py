b1819bb5fac85005e994a279d16f9bb64ee5e4e2
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-05-13 11:26:06 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -16,7 +16,8 @@ with col1:
   
 with col2:
   st.header("Sobre Mim:")
-  st.write("Olá! Eu sou Flávia e não sou formada em nada.")
+  # st.write("Olá! Eu sou Flávia e não sou formada em nada.")
+  st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
 st.write("")
   
